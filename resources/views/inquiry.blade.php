c460fee0707cc6512620d4ed50fe198165862991
younghiro <younghiromu112010@icloud.com>
2023-07-30 15:55:32 +0900

    footer

--- resources/views/inquiry.blade.php
+++ resources/views/inquiry.blade.php
@@ -112,9 +112,7 @@ text
         <div class="c-button-list">
             <div class="button_frame">
                 <div class="button_detail">
-                    <a href="{{ route('contact_confirm')">
                     <button onclick="" type="submit" class="c-button__primary ">確認する</button>
-                    </a>
                 </div>
             </div>
         </div>
